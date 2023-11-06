bcc21afe3cbbc99e67ce077af8ab67b5578c14fd
Benjamin McMahon <parachutehappens@gmail.com>
2023-11-06 15:27:47 -0800

    falcon SVGs added to client assets

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Capsules from './components/Capsules'
