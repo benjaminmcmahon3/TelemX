3ba5849172161b76c66d1c450210a9e6dacff64b
Benjamin McMahon <parachutehappens@gmail.com>
2023-11-12 13:41:34 -0800

    missionTile ordering fixed to appear chronologically from current date

--- src/client/dataHandler.js
+++ src/client/dataHandler.js
@@ -1,7 +1,8 @@
 // lsp_id=121 filters to only show SpaceX contracts
 // limit=5 sets returned query results to 5
+// lsp_id=121
   const baseUrl = `https://lldev.thespacedevs.com/2.2.0/`
-  const defualtQuery = `launch/?lsp_id=121&limit=5&`
+  const defualtQuery = `launch/?limit=5&`
   
   function getConvertCurrentDate(){
     let date = new Date()
@@ -13,7 +14,7 @@
       let gte = getConvertCurrentDate()
       console.log(`Fetching future launch data after ${gte}, from location ${locationId}`)
   
-      let fetchQuery = `pad__location=${locationId}&net__gte=${gte}`
+      let fetchQuery = `pad__location=${locationId}&net__gte=${gte}&ordering=net`
       const response = await fetch(baseUrl + defualtQuery + fetchQuery)
       const result = await response.json()
       console.log('Future results from dataHandler: ', result)
@@ -29,7 +30,7 @@
       let lte = getConvertCurrentDate()
       console.log(`Fetching past launch data before ${lte}, from location ${locationId}`)
   
-      let fetchQuery = `pad__location=${locationId}&net__lte=${lte}`
+      let fetchQuery = `pad__location=${locationId}&net__lte=${lte}&ordering=-net`
       const response = await fetch(baseUrl + defualtQuery + fetchQuery)
       const result = await response.json()
       console.log('Past results from dataHandler: ', result)
