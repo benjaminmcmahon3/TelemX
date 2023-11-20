afbc610abc3da968b5fa9dedc0af48d84061fa22
Benjamin McMahon <parachutehappens@gmail.com>
2023-11-20 11:18:34 -0800

    added react-icons, adjust tile styling

--- src/client/components/Tile.jsx
+++ src/client/components/Tile.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { convertDateFromIso } from "../dataHandler"
 import { useNavigate } from "react-router-dom"
 import './launchDisplay.css'
+import { SlCalender, SlLocationPin } from "react-icons/sl";
 
 const Tile = React.memo(({ launch }) => {
 
@@ -10,14 +11,14 @@ const Tile = React.memo(({ launch }) => {
   return(
     <div className='tileContainer'>
       <div className="tileTop">
-        <h3 className="tileDate">{
-          convertDateFromIso(launch.net)
-        }</h3>
         <h3 className="tileTitle">{launch.mission.name}</h3>
+        <h4 className="tileDate"><SlCalender /> {
+          convertDateFromIso(launch.net)
+        }</h4>
+        <h4 className="tilePad"><SlLocationPin /> {launch.pad.name}</h4>
       </div>
       <div className="tileBottom">
         <img className="tileImage" src={launch.image} alt={`Image of the ${launch.name} launch`}></img>
-        {/* <h3 className="tilePad">{launch.pad.name}</h3> */}
         <button className="tileButton" onClick={()=>{
           navigate(`/${launch.id}`)
         }}>More Info (WIP)</button>
